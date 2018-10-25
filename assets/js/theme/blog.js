822a815051359d1f887c2984ff3dcc7f5a7cee20
Corey Ellis <cellis@successpartners.com>
2018-10-24 22:20:28 -0500

    Chelsea 1.18.6

---
+++ assets/js/theme/blog.js
@@ -0,0 +1,9 @@
+import PageManager from '../pageManager';
+import 'vanilla-fitvids/jquery.fitvids';
+
+export default class Blog extends PageManager {
+  constructor() {
+    super();
+    $('.rte').fitVids();
+  }
+}
---
+++ assets/js/theme/page.js
@@ -0,0 +1,9 @@
+import PageManager from '../pageManager';
+import 'vanilla-fitvids/jquery.fitvids';
+
+export default class Page extends PageManager {
+  constructor() {
+    super();
+    $('.rte').fitVids();
+  }
+}
